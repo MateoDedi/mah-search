15d002790b2f84e194336decdca7adedcca0953f
MateoDedi <mateod@live.com>
2023-12-01 15:28:35 +0100

    deploy debug

--- app.js
+++ app.js
@@ -5,21 +5,23 @@ const dbURI = process.env.MONGODB_URI || process.env.DB_URI;
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
 const { requireAuth, checkUser } = require('./middleware/authMiddleware');
-//gg
+
 const app = express();
 
 // middleware
-app.use(express.static('public'));
+
 app.use(express.json());
 app.use(cookieParser());
 
 // view engine
-app.set('view engine', 'ejs');
+app.set("views", dirname + "/views");
+app.set("view engine", "ejs");
+app.use(express.static(dirname + "public"));
 
 // database connection
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+mongoose.connect(dbURI)
+  .then((result) => app.listen(PORT))
   .catch((err) => console.log(err));
 
 // routes
