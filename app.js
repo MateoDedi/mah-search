d56b92e87fcad7702185cc12fb30a5edc5c6ac87
MateoDedi <mateod@live.com>
2023-12-01 15:44:43 +0100

    debug css

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 // view engine
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 
 // database connection
 
