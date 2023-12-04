a7fb50ec6491662dc8c458d831968de0cf21504d
MateoDedi <mateod@live.com>
2023-12-04 17:40:23 +0100

    .

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 const app = express();
 
 // middleware
-app.use(express.static('public')); //
+//app.use(express.static('public')); //
 app.use(express.json());
 app.use(cookieParser());
 
 // view engine
-//app.set("views", __dirname + "/views");
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-//app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 // database connection
 
